f2a2aa030d1cd9a2ac7e37a5002ffa84c844ec26
Adwoa-p <pokuaaa158@gmail.com>
2024-08-28 15:52:43 +0000

    populated html table with data from db

--- app_db.py
+++ app_db.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 from markupsafe import escape
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 #configuring sqlalchemy
@@ -14,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
 
 db = SQLAlchemy(app) #to help us communicate with the database
+Migrate(app,db)
 
 #model or entity
 class Student(db.Model): 
@@ -22,14 +24,29 @@ class Student(db.Model):
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
     gender = db.Column(db.Text)
+    allowance = db.Column(db.Integer)
 
 
-    def __init__(self,name,age,gender):
+    def __init__(self,name,age,gender,allowance):
         self.name = name
         self.age = age
         self.gender = gender
+        self.allowance = allowance
+
+# to get data from the db
+    def __repr__(self):
+        return f"{self.name},{self.age},{self.gender},{self.allowance}"
 
 
-@app.route("/")
-def index():
-    return "<h1> Getting started with Flask and Databases </h1>"
+@app.route("/students")
+def get_all_students():
+    students_from_database = Student.query.all()
+    return render_template('index.html', students=students_from_database)
+
+@app.route("/students/<name>")
+def delete_students(name):
+    Student.query.filter(Student.name == name ).delete(synchronize_session=False)
+    remaining_students = Student.query.all()
+    db.session.add_all(remaining_students)
+    db.session.commit()
+    return render_template('index.html', students=remaining_students)
--- create_db.py
+++ create_db.py
@@ -4,12 +4,12 @@ with app.app_context():
     db.create_all()
 
 
-    joshua = Student('Joshua',2,'male')
-    nicole = Student('Nicole',45,'female')
-    herbert = Student('Herbert',32,'male')
-    jalilu = Student('Jalilu',12,'male')
-    pokua = Student('Pokua',15,'female')
-    albert = Student('Albert',50,'male')
+    joshua = Student('Joshua',2,'male',1000)
+    nicole = Student('Nicole',45,'female',1000)
+    herbert = Student('Herbert',32,'male',1000)
+    jalilu = Student('Jalilu',12,'male',1000)
+    pokua = Student('Pokua',15,'female',1000)
+    albert = Student('Albert',50,'male',1000)
 
     db.session.add_all([joshua,nicole,herbert,jalilu,pokua,albert])
     db.session.commit()
